e8b1c052f1fb15e0381efba3ac07aacbee0c6225
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-12-15 21:42:08 +0530

    More operations included

--- Calculator.py
+++ Calculator.py
@@ -4,4 +4,6 @@ b = 3
 print("The value of", a, "+", 3, "is:", a + b)
 print("The value of", a, "-", 3, "is:", a - b)
 print("The value of", a, "*", 3, "is:", a * b)
-print("The value of", a, "/", 3, "is:", a / b)
+print("The value of", a, "//", 3, "is:", a // b)
+print("The value of", a, "**", 3, "is:", a ** b)
+print("The value of", a, "%", 3, "is:", a % b)
