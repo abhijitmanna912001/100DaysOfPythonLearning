b3461e85988eabdfc54fcf012b5fe7c3e5411ef5
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-12-15 21:03:21 +0530

    Variables and Data Types

---
+++ 5.py
@@ -0,0 +1,11 @@
+a = 123
+b = True
+c = "Abhijit"
+d = None
+e = complex(1,2)
+print(e)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+print("The type of d is ", type(d))
+print("The type of e is ", type(e))
