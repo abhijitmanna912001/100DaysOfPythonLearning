b594b81470af1b56b55ddfdc572058dcf86171eb
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-12-16 11:46:33 +0530

    Strings in python

---
+++ 9.py
@@ -0,0 +1,15 @@
+name = "Abhijit"
+print(name)
+apple = '''He said, 
+"I want to eat an apple'''
+print("Hello, "+name)
+print(apple)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+for character in name:
+    print(character)
