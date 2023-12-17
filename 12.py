f6caef7b0ba910851cc077379f52a4281a82cec1
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-12-17 22:26:33 +0530

    If else conditionals

---
+++ 12.py
@@ -0,0 +1,16 @@
+# a = int(input("Enter your age: "))
+# print("Your age is:", a)
+
+# if (a > 18):
+#     print("You can drive")
+# else:
+#     print("You cannot drive")
+
+applePrice = 10
+budget = 200
+if (budget-applePrice > 50):
+    print("Alexa, add 1kg Apples to the cart.")
+elif (budget-applePrice > 70):
+    print("It's okay you can buy")
+else:
+    print("Alexa, do not add Apples to the cart.")
