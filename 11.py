dd0723cf54c1289736753892369ea9cc5e3b4025
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-12-17 14:59:26 +0530

    String Methods

---
+++ 11.py
@@ -0,0 +1,26 @@
+a = "Abhijit! !!!"
+print(a.upper())
+print(a.lower())
+print(a.rstrip("!"))
+print(a.replace("Abhijit", "John"))
+print(a.split(" "))
+b = "IntroductioN tO Js"
+print(b.capitalize())
+str1 = "Welcome to Abhijit"
+print(str1.center(50))
+print(a.count("Abhijit"))
+print(a.endswith("!!!"))
+print(a.find("ij"))
+# print(a.index("ijt"))
+c = "Welcome00"
+print(c.isalnum())
+print(c.isalpha())
+d = 'abhijit'
+print(a.islower())
+print(d.isprintable())
+e = "    "
+print(e.isspace())
+print(a.istitle())
+print(a.startswith("A"))
+print(str1.swapcase())
+print(str1.title())
